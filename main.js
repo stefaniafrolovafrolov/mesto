1c4468d1b9dd75ae4377265726357d7e3ae974f3
Vadim Frolov <stefaniafrolovafrolov@yandex.ru>
2022-11-24 02:17:38 +0300

    КОММИТ

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+/*Написал код для модального окна по id и классу. Не стал делать
+ удаление доп класса('.popup') в html, так как этот вариант получился оптимизированней. И + дополнительно
+ сделал закрытие модального окна по клику на дисплей вне формы. */
+
 let modal = document.getElementById("myModal");
 let button = document.getElementById("myBtn");
 let buttonDel = document.getElementsByClassName("popup__close")[0];
@@ -15,3 +19,22 @@ window.onclick = function (event) {
     modal.style.display = "none"
   }
 }
+
+let formElement = document.querySelector(".popup__form"); 
+const nameInput = document.querySelector(".form__input_type_name");
+const jobInput = document.querySelector(".form__input_type_job");
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+
+    nameInput.value = "";
+    jobInput.value = "";
+    nameInput.textContent = "";
+
+
+
+}
+
+
+formElement.addEventListener('submit', formSubmitHandler); 
+console.log(nameInput.value);
+console.log(jobInput.value);

ea24e4c31ab35935f36b23e918f04cb677009b14
Vadim Frolov <stefaniafrolovafrolov@yandex.ru>
2022-11-29 13:56:54 +0300

    Project Mesto работаю над 5 проектом

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,68 @@ let jobInput = document.querySelector(".popup__input_type_job")
 let profTitle = document.querySelector(".profile__title")
 let profParag = document.querySelector(".profile__subtitle")
 
+let formElementNew = document.querySelector(".popup__form-new")
+let profAddBtn = document.querySelector(".profile__add-button")
+let popupNew = document.querySelector(".popup-new")
+let closeNewBtn = document.querySelector(".popup__close-new")
+let elements = document.querySelector(".elements")
+let likeBtn = document.querySelector(".element__like-button")
+
+
+
+
+
+
+
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  },
+ 
+];
+
+
+/*elements.insertAdjacentHTML('beforeend', ``);
+
+*/
+/*function colorLike() {
+  likeBtn.classList.add("element__like-button_active")
+ console.log(likeBtn);
+}
+*/
+
+function openPopupNew() {
+  popupNew.classList.add("popup-new_opened")
+ console.log(popupNew);
+}
+
+function closePopupAdd() {
+  popupNew.classList.remove("popup-new_opened")
+  console.log(closeNewBtn);
+}
+
 function openPopup() {
   popup.classList.add("popup_opened")
   nameInput.value = profTitle.textContent
@@ -24,6 +86,22 @@ function sendForm(evt) {
   closePopup()
 }
 
+function colorActive() {
+  likeBtn.classList.add("element__like-button_active")
+ 
+}
+
+function closeColor() {
+  likeBtn.classList.remove("element__like-button_active")
+}
+
+
+
+
+closeNewBtn.addEventListener("click", closePopupAdd)
+profAddBtn.addEventListener("click", openPopupNew)
 editBtn.addEventListener("click", openPopup)
 closeBtn.addEventListener("click", closePopup)
 formElement.addEventListener("submit", sendForm)
+likeBtn.addEventListener("click", colorActive)
+likeBtn.addEventListener("click", closeColor)

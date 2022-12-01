ec109bb1694696c9dd1146386dad579e4a0d70d4
Vadim Frolov <stefaniafrolovafrolov@yandex.ru>
2022-12-01 14:57:32 +0300

    Project Mesto работаю над 5 проектом

--- scripts/index.js
+++ scripts/index.js
@@ -12,7 +12,19 @@ let profAddBtn = document.querySelector(".profile__add-button")
 let popupNew = document.querySelector(".popup-new")
 let closeNewBtn = document.querySelector(".popup__close-new")
 let elements = document.querySelector(".elements")
-let likeBtn = document.querySelector(".element__like-button")
+/*let likeBtn = document.querySelector(".element__like-button")*/
+
+
+
+
+let nameInputNew = document.querySelector(".popup__input_type_name")
+let jobInputNew = document.querySelector(".popup__input_type_job")
+
+
+
+
+
+
 
 let element = document.querySelector(".elements")
 let div = document.createElement("div")
@@ -42,19 +54,17 @@ divThree.appendChild(btnTrashThree)
 let image = document.createElement("img")
 image.classList.add("element__mask")
 image.src =
-  "https://images.unsplash.com/photo-1630011575551-a07690be4c4d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fCVEMCVCMCVEMSU4MCVEMSU4NSVEMSU4QiVEMCVCN3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+  "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg"
 div.appendChild(image)
 
 let imageTwo = document.createElement("img")
 imageTwo.classList.add("element__mask")
-imageTwo.src =
-  "https://images.unsplash.com/photo-1609067936529-59bf24113fec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fCVEMSU4NyVEMCVCNSVEMCVCQiVEMSU4RiVEMCVCMSVEMCVCOCVEMCVCRCVEMCVCQSVEMCVCMCVEMSU4RiUyMCVEMCVCRSVEMCVCMSVEMCVCQiVEMCVCMCVEMSU4MSVEMSU4MiVEMSU4Q3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+imageTwo.src = "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg"
 divTwo.appendChild(imageTwo)
 
 let imageThree = document.createElement("img")
 imageThree.classList.add("element__mask")
-imageThree.src =
-  "https://images.unsplash.com/photo-1598385716618-d0dbc5f9ebca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8JUQwJUI4JUQwJUIyJUQwJUIwJUQwJUJEJUQwJUJFJUQwJUIyJUQwJUJFfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+imageThree.src = "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg"
 divThree.appendChild(imageThree)
 
 let divGroup = document.createElement("div")
@@ -107,51 +117,65 @@ divGroupThree.appendChild(btnLikeThree)
 
 
 
-console.log(element)
+
 
 
 const initialCards = [
-  {
+ div = {
     name: "Архыз",
     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
   },
-  {
+  divTwo = {
     name: "Челябинская область",
     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
   },
-  {
+  divThree = {
     name: "Иваново",
     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
   },
-  {
-    name: "Камчатка",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-  },
-  {
-    name: "Холмогорский район",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-  },
-  {
-    name: "Байкал",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-  },
+  
 ]
 
-/*elements.insertAdjacentHTML('beforeend', ``);
 
-*/
-/*function colorLike() {
-  likeBtn.classList.add("element__like-button_active")
- console.log(likeBtn);
+
+
+
+console.log(div)
+console.log(divTwo)
+console.log(divThree)
+
+
+function btnTrashDel() {
+  btnTrash.parentElement.remove(div)
 }
-*/
+
+function btnTrashTwoDel() {
+  btnTrashTwo.parentElement.remove(divTwo)
+}
+
+function btnTrashThreeDel() {
+  btnTrashThree.parentElement.remove(divThree)
+}
+
+
+
+
+
+
+
+
+
+
+
 
 function openPopupNew() {
   popupNew.classList.add("popup-new_opened")
+  nameInputNew.value = div.parentElement
+  jobInputNew.value = div.parentElement
   console.log(popupNew)
 }
 
-function closePopupAdd() {
+function closePopupNew() {
   popupNew.classList.remove("popup-new_opened")
   console.log(closeNewBtn)
 }
@@ -173,18 +197,59 @@ function sendForm(evt) {
   closePopup()
 }
 
-function colorActive() {
-  likeBtn.classList.add("element__like-button_active")
+
+function sendFormNew(evt) {
+  evt.preventDefault()
+  div.parentElement = nameInputNew.value
+  div.parentElement = jobInputNew.value
+  closePopupNew()
 }
 
-function closeColor() {
-  likeBtn.classList.remove("element__like-button_active")
+
+
+/*let likeBtn = document.querySelectorAll(".element__like-button")
+for(let i=0; i<likeBtn.length; i++){
+  likeBtn[i].addEventListener('click', function(e){
+    likeBtn[i].classList.add("element__like-button_active") 
+    /*console.log(e.target.getAttribute('id'));*//*
+  })
+}
+*/
+
+
+function ready() {
+  let likeBtn = document.getElementsByClassName("element__like-button");
+ for (let i = 0; likeBtn.length > i; i++) {
+    likeBtn[i].onclick = function() {
+          for (i = 0; likeBtn.length > i; i++) {
+            likeBtn[i].classList.add("element__like-button");
+          }
+          if (this.classList.contains("element__like-button_active")) {
+            this.classList.remove("element__like-button_active")
+           } else {
+            this.classList.add("element__like-button_active")
+          }
+      };
+  }
 }
 
-closeNewBtn.addEventListener("click", closePopupAdd)
+document.addEventListener("click", ready);
+
+
+
+
+
+closeNewBtn.addEventListener("click", closePopupNew)
 profAddBtn.addEventListener("click", openPopupNew)
 editBtn.addEventListener("click", openPopup)
 closeBtn.addEventListener("click", closePopup)
 formElement.addEventListener("submit", sendForm)
-likeBtn.addEventListener("click", colorActive)
-likeBtn.addEventListener("click", closeColor)
+
+/*formElementNew.addEventListener("submit", )*/
+
+
+
+btnTrash.addEventListener("click", btnTrashDel)
+btnTrashTwo.addEventListener("click", btnTrashTwoDel)
+btnTrashThree.addEventListener("click", btnTrashThreeDel)
+

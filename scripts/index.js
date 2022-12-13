58ca8b7d6c32d82f555e2f6c493e6e8f455af9ac
Vadim Frolov <stefaniafrolovafrolov@yandex.ru>
2022-12-13 11:28:17 +0300

    Project Mesto переделка попапов

--- scripts/index.js
+++ scripts/index.js
@@ -1,54 +1,31 @@
-const popups = document.querySelectorAll(".popup")
-const popupEditProfile = document.querySelector(".popup_type_edit-profile");
-const popupAddCard = document.querySelector(".popup_type_add-card");
-
-
-
-
 const formProfile = document.querySelector("#editForm")
 const popupProfileOpenButton = document.querySelector(".profile__edit-button")
 const popupProfileCloseButton = document.querySelector(".popup__close")
-/*const popupProfile = document.querySelector(".popup")*/
+const popupProfile = document.querySelector(".popup")
 const nameInput = document.querySelector(".popup__input_type_name")
 const jobInput = document.querySelector(".popup__input_type_job")
 const profileTitle = document.querySelector(".profile__title")
 const profileParag = document.querySelector(".profile__subtitle")
 const popupProfileOpenButtonNew = document.querySelector(".profile__add-button")
-/*const popupProfileNew = document.querySelector(".popup")*/
+const popupProfileNew = document.querySelector(".popup-new")
 const popupProfileCloseButtonNew = document.querySelector(".popup-new__close-new")
 
-
-const openPopup = (popup) => {
-  popup.classList.add("popup_opened");
-};
-const closePopup = (popup) => {
-  popup.classList.remove("popup_opened");
-};
-popups.forEach((popup) => {
-  popup.addEventListener("click", (e) => {
-    if (e.target.classList.contains("popup__close")) closePopup(popup);
-  });
-});
-
-
-function openEditProfile() {
-  openPopup(popupEditProfile);
-  nameInput.value = profileTitle.textContent;
-  jobInput.value = profileParag.textContent;
+function openPopupNew() {
+  popupProfileNew.classList.add("popup-new_opened")
 }
-popupEditProfile.addEventListener("click", openPopup)
-popupProfileCloseButton.addEventListener("click", closePopup)
-/*function openPopup() {
-  popups.classList.add("popup_opened")
+
+function openPopup() {
+  popupProfile.classList.add("popup_opened")
   nameInput.value = profileTitle.textContent
   jobInput.value = profileParag.textContent
-}*/
-
-/*function closePopup() {
-  popupProfile.classList.remove("popup_opened")
- 
 }
-*/
+
+function closePopup() {
+  popupProfile.classList.remove("popup_opened")
+  popupProfileNew.classList.remove("popup-new_opened")
+  popupImage.classList.remove("image-popup_opened")
+}
+
 function submitProfileForm(evt) {
   evt.preventDefault()
   profileTitle.textContent = nameInput.value
@@ -56,26 +33,14 @@ function submitProfileForm(evt) {
   closePopup()
 }
 
-
-
-/*popupProfileCloseButtonNew.addEventListener("click", closePopup)
+popupProfileCloseButtonNew.addEventListener("click", closePopup)
 popupProfileOpenButton.addEventListener("click", openPopup)
+popupProfileOpenButtonNew.addEventListener("click", openPopupNew)
 popupProfileCloseButton.addEventListener("click", closePopup)
-formProfile.addEventListener("submit", submitProfileForm)*/
+formProfile.addEventListener("submit", submitProfileForm)
 
 const cardsContainer = document.querySelector(".elements")
 const template = document.querySelector("#element-template").content
-
-const placeInfo = initialCards.map(function (item) {
-  return {
-    name: item.name,
-    link: item.link,
-  }
-})
-
-function render() {
-  placeInfo.reverse().forEach(renderCard)
-}
 
 const imageOpenPopup = document.querySelector(".image-popup__container")
 const popupImage = document.querySelector(".image-popup")
@@ -91,35 +56,47 @@ function imageOpen(card, link) {
   popupImage.classList.add("image-popup_opened")
 }
 
-function renderCard({ name, link }) {
+function createCard(value) {
   const card = template.querySelector(".element").cloneNode(true)
-  card.querySelector(".element__title").textContent = name
-  card.querySelector(".element__mask").src = link
+  if (card) {
+    const title = card.querySelector(".element__title")
+    const mask = card.querySelector(".element__mask")
+    const trash = card.querySelector(".element__trash")
+    const like = card.querySelector(".element__like-button")
+    if (title && mask && trash && like) {
+      title.textContent = value.name
+      mask.src = value.link
+      mask.addEventListener("click", () => {
+        imageOpen(card, value.link)
+      })
+      trash.addEventListener("click", () => {
+        card.remove()
+      })
 
-  card.querySelector(".element__mask").addEventListener("click", () => {
-    imageOpen(card, link)
-  })
-
-  card.querySelector(".element__trash").addEventListener("click", () => {
-    card.remove()
-  })
-
-  card.querySelector(".element__like-button").addEventListener("click", () => {
-    if (
-      card
-        .querySelector(".element__like-button")
-        .classList.contains("element__like-button_active")
-    ) {
-      card
-        .querySelector(".element__like-button")
-        .classList.remove("element__like-button_active")
-    } else {
-      card
-        .querySelector(".element__like-button")
-        .classList.add("element__like-button_active")
+      like.addEventListener("click", () => {
+        const button = card.querySelector(".element__like-button")
+        const clsName = "element__like-button_active"
+        if (button)
+          if (button.classList.contains(clsName)) {
+            button.classList.remove(clsName)
+          } else {
+            button.classList.add(clsName)
+          }
+      })
     }
+  }
+  return card
+}
+
+function renderCard(card, container) {
+  container.prepend(card)
+}
+
+function render() {
+  initialCards.reverse().forEach((value) => {
+    const newCard = createCard(value)
+    if (newCard) renderCard(newCard, cardsContainer)
   })
-  cardsContainer.prepend(card)
 }
 
 render()
@@ -132,7 +109,8 @@ function submitCardForm(evt) {
   evt.preventDefault()
   const name = nameInputNew.value
   const link = linkInputNew.value
-  renderCard({ name, link })
+  const newCard = createCard({ name, link })
+  if (newCard) renderCard(newCard, cardsContainer)
   closePopup()
   formProfileNew.reset()
 }

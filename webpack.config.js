a42076275df51a82c534a23477a34f1ef9809776
Vadim Frolov <stefaniafrolovafrolov@yandex.ru>
2023-02-09 15:14:41 +0300

    тестовая ветка для повтора материала по js

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
 const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 
 module.exports = {
   mode: "development",
   entry: {
     index: path.resolve(__dirname, "src", "pages", "index.js"),
   },
-  // devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   output: {
     filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "dist"),
@@ -45,14 +46,22 @@ module.exports = {
       },
     ],
   },
+
   plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+  ],
+  /*[
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "src", "index.html"),
     }),
     new MiniCssExtractPlugin(),
-  ],
+  ]*/
   devServer: {
-    static: path.resolve(__dirname, './dist'),
+    static: path.resolve(__dirname, "./dist"),
     port: 8080,
     open: true,
     hot: true,
